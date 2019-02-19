73e5b8ca56225b425a92a2848034b2acb9c2d9d3
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-19 07:37:31 +0100

    Updated extension.ts to make use of CommandManager to register commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CommandManager } from './commands/command-manager';
+import container from './inversify.config';
+import TYPES from './types';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "clipimg-vscode" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello gep13 stream!');
-	});
-
-	context.subscriptions.push(disposable);
+	const cmdManager = container.get<CommandManager>(TYPES.CommandManager);
+	cmdManager.registerCommands(context);
 }
 
 // this method is called when your extension is deactivated
