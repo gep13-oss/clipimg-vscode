8807ba1b0fd0af80ff8c695495fc23df2aa5e2ad
Gary Ewan Park <gep13@gep13.co.uk>
2019-04-19 22:01:17 +0100

    (GH-4) Switch to using Chocolatey Extension

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,10 +1,4 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-. "$toolsDir\Install-VsCodeExtension.ps1"
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  extensionPath = "$toolsDir\clipimg-vscode.vsix"
-}
-
-Install-VsCodeExtension @packageArgs
+Install-VsCodeExtension "$toolsDir\clipimg-vscode.vsix"
